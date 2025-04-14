33bbdf2b79e94d49fd906c4637e15958d329455a
Loh10 <naturellement.toi@gmail.com>
2025-04-14 11:32:53 +0200

    Spawn Without team

--- GameFiles/Loot-Saloon/Assets/Player/S_PlayersSpawner.cs
+++ GameFiles/Loot-Saloon/Assets/Player/S_PlayersSpawner.cs
@@ -1,45 +1,62 @@
-using System.Collections.Generic;
+#region
+using Unity.Netcode;
 using UnityEngine;
+#endregion
 
 public enum Team // It Exist somewere else Please change with the Adao's Enum /!\
 {
-    Red, Blue
+    Red,
+    Blue
 }
 
 public class S_PlayersSpawner : MonoBehaviour
 {
     [SerializeField] private Transform _redTeam;
     [SerializeField] private Transform _blueTeam;
-    public List<GameObject> p1;
-    public List<GameObject> p2;
 
     [SerializeField] private float _spawnDistance = 5f;
     [SerializeField] private float _SpawnRadius = 10f;
 
-    public void SpawnPlayer(List<GameObject> p_players, Transform p_origin)
+    public static S_PlayersSpawner Instance { get; private set; }
+
+    private void Awake()
     {
-        int count = p_players.Count;
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SpawnPlayer(GameObject p_player, Transform p_origin)
+    {
+        Debug.Log($"[{nameof(S_PlayersSpawner)}] Spawn player {p_player.name} at {p_origin.position}");
+        int count = NetworkManager.Singleton.ConnectedClients.Count;
 
         float totalWidth = (count - 1) * _spawnDistance;
         float startX = p_origin.position.x - totalWidth / 2f;
         float fixedZ = p_origin.position.z;
-
-        for (int i = 0; i < count; i++)
+        NetworkObject networkObject = p_player.TryGetComponent(out NetworkObject netObj) ? netObj : null;
+        if (networkObject == null)
         {
-            GameObject player = p_players[i];
-
-            Vector3 pos = new Vector3(startX + i * _spawnDistance, p_origin.position.y, fixedZ);
-            player.transform.position = pos;
-
-            player.transform.rotation = Quaternion.LookRotation(Vector3.right);
+            Debug.LogError($"[{nameof(S_PlayersSpawner)}] NetworkObject not found on {p_player.name}");
+            return;
         }
+
+        Vector3 pos = new Vector3(startX + p_player.GetComponent<NetworkObject>().NetworkObjectId * _spawnDistance, p_origin.position.y, fixedZ);
+        p_player.transform.position = pos;
+
+        p_player.transform.rotation = Quaternion.LookRotation(Vector3.right);
     }
 
     public void RandomSpawnInRadius(GameObject p_player, Transform p_origin)
     {
         const int maxAttempts = 30;
         float radius = _SpawnRadius;
-        float checkRadius = 0.5f; 
+        float checkRadius = 0.5f;
 
         for (int attempt = 0; attempt < maxAttempts; attempt++)
         {
@@ -60,6 +77,4 @@ public class S_PlayersSpawner : MonoBehaviour
 
         Debug.LogWarning($"[{nameof(S_PlayersSpawner)}] Can't find a safe zone after {maxAttempts} tries.");
     }
-
-
 }
