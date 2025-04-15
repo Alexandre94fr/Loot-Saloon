9fa8b4089637091b350d0b71fba0b8f17ec20e58
Alexandre <Alexandre94.fr@gmail.com>
2025-04-15 12:09:26 +0200

    ♻️ ▸ Cleaning the S_Quato Script

--- GameFiles/Loot-Saloon/Assets/Loot/S_Quota.cs
+++ GameFiles/Loot-Saloon/Assets/Loot/S_Quota.cs
@@ -2,34 +2,34 @@ using UnityEngine;
 
 public class S_Quota : MonoBehaviour
 {
+    public int total { get; private set; }
+    public int quota { get; private set; }
+
     [Header(" External references :")]
-    [SerializeField] private S_LootInstantiator _instantiator;
+    [SerializeField] private S_LootInstantiator _lootInstantiatorInstance;
 
     [Header(" Properties :")]
     [Tooltip("How much of the total sum the quota is equal to")]
-    [SerializeField] [Range(0f, 1f)] private float _fraction = 0.67f;
+    [SerializeField] [Range(0f, 1f)] private float _extractionQuotaRatio = 0.67f;
 
-    public int total {get; private set;}
-    public int quota {get; private set;}
 
     private void Start()
     {
-        if (!_instantiator)
+        if (!_lootInstantiatorInstance)
         {
             Debug.LogError(
-                $"ERROR ! The '{nameof(_instantiator)}' variable was not set, the value is null. " +
+                $"ERROR ! The '{nameof(_lootInstantiatorInstance)}' variable of the '{nameof(name)}' GameObject was not set, the value is null. " +
                 $"Stopping Start method execution"
             );
             return;
         }
 
-        _instantiator.OnVaultFilled.AddListener(OnVaultFilled);
+        _lootInstantiatorInstance.OnVaultFilled.AddListener(OnVaultFilled);
     }
     
-    public void OnVaultFilled(S_BankVault vault)
+    public void OnVaultFilled(S_BankVault p_bankVault)
     {
-        total += vault.moneyValue;
-        quota = (int) (total * _fraction);
-
+        total += p_bankVault.moneyValue;
+        quota = (int)(total * _extractionQuotaRatio);
     }
 }
