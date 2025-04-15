bd79df7ad251fc9d2b215c2d401143350f9957f8
Alexandre <Alexandre94.fr@gmail.com>
2025-04-15 11:29:16 +0200

    🔧 ▸ Removing runtime game errors

--- GameFiles/Loot-Saloon/Assets/Loot/S_Quota.cs
+++ GameFiles/Loot-Saloon/Assets/Loot/S_Quota.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class S_Quota : MonoBehaviour
 {
+    [Header(" External references :")]
     [SerializeField] private S_LootInstantiator _instantiator;
+
+    [Header(" Properties :")]
     [Tooltip("How much of the total sum the quota is equal to")]
     [SerializeField] [Range(0f, 1f)] private float _fraction = 0.67f;
 
@@ -11,6 +14,15 @@ public class S_Quota : MonoBehaviour
 
     private void Start()
     {
+        if (!_instantiator)
+        {
+            Debug.LogError(
+                $"ERROR ! The '{nameof(_instantiator)}' variable was not set, the value is null. " +
+                $"Stopping Start method execution"
+            );
+            return;
+        }
+
         _instantiator.OnVaultFilled.AddListener(OnVaultFilled);
     }
     
