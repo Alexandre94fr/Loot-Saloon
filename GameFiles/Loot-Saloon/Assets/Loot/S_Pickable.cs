95c23f52d85d59c268b83983100a91ac9e2d2574
Alexandre <Alexandre94.fr@gmail.com>
2025-04-22 10:37:35 +0200

    🔧 ▸ Fixing critical bug

--- GameFiles/Loot-Saloon/Assets/Loot/S_Pickable.cs
+++ GameFiles/Loot-Saloon/Assets/Loot/S_Pickable.cs
@@ -34,7 +34,7 @@ public abstract class S_Pickable : S_Interactable
     public override void StopInteract(S_PlayerInteract p_playerInteract)
     {
         _isPickUp = false;
-        S_PlayerUseUI.OnCircleChange(0);
+        S_PlayerUseUI.OnCircleChange?.Invoke(0);
     }
 
     public override void Interact(S_PlayerInteract p_playerInteract, Transform p_parent)
