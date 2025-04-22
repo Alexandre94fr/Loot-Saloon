8a36782dc9ea0adee457032768c6d9b780499c26
Alexandre <Alexandre94.fr@gmail.com>
2025-04-22 11:58:04 +0200

    ♻️ ▸ Renaming, replacing the S_UseFullShortcuts Script

--- GameFiles/Loot-Saloon/Assets/ExternalTools/UsefulShortcuts/Editor/S_UsefulShortcuts.cs
+++ GameFiles/Loot-Saloon/Assets/ExternalTools/UsefulShortcuts/Editor/S_UsefulShortcuts.cs
@@ -3,15 +3,16 @@ using UnityEditor;
 using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
-static class UsefulShortcuts
+static class S_UsefulShortcuts
 {
     // Alt + C
     [Shortcut("Clear Console", KeyCode.C, ShortcutModifiers.Alt)]
     public static void ClearConsole()
     {
-        var assembly = Assembly.GetAssembly(typeof(SceneView));
-        var type = assembly.GetType("UnityEditor.LogEntries");
-        var method = type.GetMethod("Clear");
+        Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+        System.Type type = assembly.GetType("UnityEditor.LogEntries");
+        MethodInfo method = type.GetMethod("Clear");
+
         method.Invoke(new object(), null);
     }
 }
