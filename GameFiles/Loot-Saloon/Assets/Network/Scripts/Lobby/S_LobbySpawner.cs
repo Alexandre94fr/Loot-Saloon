3c28a6accb522643c16ddbc5d78ae2653380367b
Loh10 <naturellement.toi@gmail.com>
2025-04-23 14:37:36 +0200

    main Menu

--- GameFiles/Loot-Saloon/Assets/Network/Scripts/Lobby/S_LobbySpawner.cs
+++ GameFiles/Loot-Saloon/Assets/Network/Scripts/Lobby/S_LobbySpawner.cs
@@ -23,7 +23,7 @@ public class S_LobbySpawner : MonoBehaviour
     private void OnLobbyUpdated(Lobby p_lobby)
     {
         List<S_LobbyPlayerData> playersData = S_GameLobbyManager.instance.GetPlayers();
-
+        Debug.Log("OnLobbyUpdated");
         for (int i = 0; i < playersData.Count; i++)
         {
             S_LobbyPlayerData playerData = playersData[i];
