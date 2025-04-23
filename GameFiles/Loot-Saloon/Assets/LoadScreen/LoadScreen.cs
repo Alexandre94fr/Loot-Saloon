0f2ac5640720d42a6797962dabfc0087f9d05305
Loh10 <naturellement.toi@gmail.com>
2025-04-23 15:44:25 +0200

    add loading Circle Visual on button And Quit Function

--- GameFiles/Loot-Saloon/Assets/LoadScreen/LoadScreen.cs
+++ GameFiles/Loot-Saloon/Assets/LoadScreen/LoadScreen.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections;
 using Unity.Services.Lobbies.Models;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class LoadScreen : MonoBehaviour
 {
+    [SerializeField] private RectTransform _rectComponent;
+    [SerializeField] private float _rotateSpeed = 200f;
+
     private void OnEnable()
     {
         S_LobbyEvents.OnLobbyUpdatedWithParam += OnLobbyLoaded;
@@ -24,6 +28,10 @@ public class LoadScreen : MonoBehaviour
         yield return new WaitForSeconds(2f);
         gameObject.SetActive(false);
         S_LobbyEvents.OnLobbyUpdatedWithParam -= OnLobbyLoaded;
+    }
 
+    private void Update()
+    {
+        _rectComponent.Rotate(0f, 0f, _rotateSpeed * Time.deltaTime);
     }
 }
